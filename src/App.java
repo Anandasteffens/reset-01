f25ded78ad36efd2909195037aaf88510b7f646e
Anandasteffens <ananda.steffens@hotmail.com>
2020-03-07 12:13:07 -0300

    Removendo repeticao

--- src/App.java
+++ src/App.java
@@ -2,8 +2,5 @@ public class App {
 
     public static void main (String[] args){
         System.out.println("Hello World");
-        System.out.println("Ola mundo");
-        System.out.println("Hello World");
-        System.out.println("Ola mundo");
     }
 }
