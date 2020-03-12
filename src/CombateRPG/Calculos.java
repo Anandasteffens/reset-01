7a01fd0f23ed868740afba5dbdb767eaed70f9f4
Anandasteffens <61908510+Anandasteffens@users.noreply.github.com>
2020-03-12 08:44:31 -0300

    retirar cogido inutil

--- src/CombateRPG/Calculos.java
+++ src/CombateRPG/Calculos.java
@@ -2,13 +2,6 @@ package CombateRPG;
 
 
 public class Calculos {
-/*
-    int poderFinalAtaqueEspada (int poderEspada, int ataqueAtacante) {return poderEspada*ataqueAtacante;}
-    int poderFinalAtaqueMartelo (int poderMartelo, int ataqueAtacante) {return poderMartelo*ataqueAtacante;}
-    int poderFinalAtaqueMagia (int poderMagia, int ataqueAtacante) {return poderMagia*ataqueAtacante;}
-    int poderFinalAtaqueFe (int poderFe, int ataqueAtacante) {return poderFe*ataqueAtacante;}
-    int danoFinal (int poderFinalAtaque, int defesaAlvo) {return poderFinalAtaque*defesaAlvo;}
-    */
 
     int poderFinalAtaque (int poder, int ataque) {return poder * ataque;}
 
